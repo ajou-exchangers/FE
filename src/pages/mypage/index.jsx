21ba50de817053173b8d71d03e08b0c4be079735
Davin <113905620+vincci662@users.noreply.github.com>
2023-11-17 11:09:38 +0900

    Add MyPage
    Add only simple ui to check user profile

---
+++ src/pages/mypage/index.jsx
@@ -0,0 +1,160 @@
+import React, { useState, useEffect } from 'react';
+import { Global, css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  max-width: 800px;
+`;
+
+const LeftNavbar = styled.div`
+  width: 200px;
+  height: 450px;
+  background-color: #2b2144;
+  color: #fff;
+  padding: 20px;
+  padding-top: 2.5rem;
+`;
+
+const ContentContainer = styled.div`
+  flex-grow: 1;
+  padding: 20px;
+  padding-top: 2.5rem;
+`;
+
+const NavLink = styled.a`
+  display: block;
+  color: #fff;
+  text-decoration: none;
+  margin-bottom: 10px;
+  padding: 8px;
+  border-radius: 4px;
+  &:hover {
+    background-color: #483d69;
+  }
+`;
+
+const ProfileImage = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const UserInfo = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Label = styled.span`
+  font-weight: bold;
+  color: #333;
+  margin-right: 8px;
+`;
+
+const Value = styled.span`
+  color: #555;
+`;
+
+const Button = styled.button`
+  color: #000000;
+  border: none;
+  padding: 15px 10px;
+  cursor: pointer;
+  background-image: url("./pen-solid.svg");
+  background-repeat: no-repeat;
+  margin-top: 5px;
+  background-position: center;
+  background-color: white;
+`;
+
+const MyPage = ({ userId }) => {
+  const [selectedTab, setSelectedTab] = useState('profile');
+  const dummyUserData = {
+    userId: 'davin',
+    email: 'davin@naver.com',
+    nickname: 'davini',
+    profilePicture: '/public/lock-solid.svg',
+  };
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    setUser(dummyUserData);
+  }, [userId]);
+
+  const handleTabChange = (tab) => {
+    setSelectedTab(tab);
+  };
+
+  const handleEditProfile = () => {
+    //
+  };
+
+  const renderContent = () => {
+    switch (selectedTab) {
+      case 'profile':
+        return (
+          <div>
+            <h2>My Profile <Button onClick={handleEditProfile}></Button></h2>
+            {user && (
+              <div>
+                <ProfileImage src={user.profilePicture} alt="Profile" />
+                <UserInfo>
+                  <Label>Email</Label>
+                  <Value>{user.email}</Value>
+                </UserInfo>
+
+                <UserInfo>
+                  <Label>Nickname</Label>
+                  <Value>{user.nickname}</Value>
+                </UserInfo>
+
+                <UserInfo>
+                  <Label>Campus</Label>
+                  <Value>Ajou Univ</Value>
+                </UserInfo>
+
+              </div>
+            )}
+          </div>
+        );
+      case 'posts':
+        return (
+          <div>
+            <h2>My Posts</h2>
+
+          </div>
+        );
+      case 'comments':
+        return (
+          <div>
+            <h2>My Comments</h2>
+
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <>
+      <Global styles={css`
+          body {
+            font-family: 'Roboto', sans-serif;
+          }
+        `}
+      />
+      <Container>
+        <LeftNavbar>
+          <NavLink href="#" onClick={() => handleTabChange('profile')}>My Profile</NavLink>
+          <NavLink href="#" onClick={() => handleTabChange('posts')}>My Posts</NavLink>
+          <NavLink href="#" onClick={() => handleTabChange('comments')}>My Comments</NavLink>
+        </LeftNavbar>
+        <ContentContainer>{renderContent()}</ContentContainer>
+      </Container>
+    </>
+  );
+};
+
+export default MyPage;
