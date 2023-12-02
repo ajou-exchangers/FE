62a8a0468f40bd579f086e233646609f11d17776
Davin <113905620+vincci662@users.noreply.github.com>
2023-12-02 10:09:52 +0900

    feat: nickname duplication button

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import styled from '@emotion/styled';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const FormContainer = styled.div`
   text-align: center;
@@ -61,45 +62,98 @@ const LinkStyled = styled(Link)`
   font-size: 0.8em;
 `;
 
+const CheckAvailabilityButton = styled(Button)`
+  font-size: 0.6em;
+  padding: 8px 16px;
+`;
+
+const ResultMessage = styled.p`
+  font-size: 0.8em;
+  color: ${({ success }) => (success ? 'green' : 'red')};
+  margin-top: 4px;
+`;
+
+
 const SignupPage = () => {
-  const { handleSubmit, control } = useForm();
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    profileImage: null,
-    profileImagePreview: null,
+  const { handleSubmit, control, watch, setError, formState: { errors } } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      nickname: '',
+    },
   });
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const [isPasswordValid, setPasswordValid] = useState(true);
+  const navigate = useNavigate();
+
+  const password = watch('password');
+  const confirmPassword = watch('confirmPassword');
+
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    const isValid = newPassword.length >= 6 && /[!@#$%^&*(),.?":{}|<>]/.test(newPassword);
+    setPasswordValid(isValid);
   };
 
-  const handleInputChange = (e) => {
-    const { name, value, type } = e.target;
-    if (type === 'file') {
-      const file = e.target.files[0];
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        [name]: file,
-        profileImagePreview: URL.createObjectURL(file),
-      }));
-    } else {
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        [name]: value,
-      }));
+  const validateEmail = (email) => {
+    const isValid = email.endsWith('@ajou.ac.kr');
+    if (!isValid) {
+      setError('email', { type: 'manual', message: 'Email must end with @ajou.ac.kr' });
+    }
+    return isValid;
+  };
+
+  const onSubmit = async (data) => {
+    try {
+      console.log(data);
+      if (!isPasswordValid || !validateEmail(data.email)) {
+        console.error('Invalid input. Please check the form fields.');
+        return;
+      }
+      const response = await axios.get(`http://15.165.42.212:3000/api/exchangers/v1/auth/${data.nickname}`);
+      const isNicknameAvailable = response.data.available;
+
+      if (!isNicknameAvailable) {
+        console.error('Nickname is not available. Please choose a different one.');
+        return;
+      }
+
+      const signupResponse = await axios.post('http://15.165.42.212:3000/api/exchangers/v1/auth/signup', {
+        email: data.email,
+        password: data.password,
+        confirmPassword: data.confirmPassword,
+        nickname: data.nickname,
+      });
+      alert('Signup successful!');
+      navigate('/');
+    } catch (error) {
+      console.error('Error during signup:', error.message);
     }
   };
 
+
+  const [nicknameAvailability, setNicknameAvailability] = useState(null);
+
+  const handleNicknameAvailabilityCheck = async () => {
+    const nickname = watch('nickname');
+
+    try {
+      const response = await axios.get(`http://example.com/check-nickname?nickname=${nickname}`);
+      const isAvailable = response.data.available;
+      setNicknameAvailability(isAvailable);
+    } catch (error) {
+      console.error('Error checking nickname availability:', error.message);
+    }
+  };
+
+
   return (
     <FormContainer>
-
       <h1>Sign up</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <FormGroup>
-          <Label>
+          <Label className="input-file-button" htmlFor="input-file">
             <img
               src="envelope-regular.svg"
               width="20"
@@ -115,10 +169,13 @@ const SignupPage = () => {
                   type="email"
                   {...field}
                   placeholder="example@ajou.ac.kr"
-                  onChange={handleInputChange}
+                  onBlur={(e) => validateEmail(e.target.value)}
                 />
               )}
             />
+            {errors.email && (
+              <p style={{ color: 'red', fontSize: '0.8em' }}>{errors.email.message}</p>
+            )}
           </Label>
         </FormGroup>
         <FormGroup>
@@ -138,11 +195,19 @@ const SignupPage = () => {
                   type="password"
                   {...field}
                   placeholder="Password at least 6 characters"
-                  onChange={handleInputChange}
+                  onChange={(e) => {
+                    field.onChange(e);
+                    handlePasswordChange(e);
+                  }}
                 />
               )}
             />
           </Label>
+          {!isPasswordValid && (
+            <p style={{ color: 'red', fontSize: '0.8em' }}>
+              Password must be at least 6 characters long and contain at least one special character.
+            </p>
+          )}
         </FormGroup>
         <FormGroup>
           <Label>
@@ -161,7 +226,6 @@ const SignupPage = () => {
                   type="password"
                   {...field}
                   placeholder="Re-enter Password"
-                  onChange={handleInputChange}
                 />
               )}
             />
@@ -177,21 +241,41 @@ const SignupPage = () => {
               className="input-icon"
             />
             <Controller
-              name="username"
+              name="nickname"
               control={control}
               render={({ field }) => (
                 <Input
                   type="text"
                   {...field}
                   placeholder="Your Nickname"
-                  onChange={handleInputChange}
                 />
               )}
             />
           </Label>
+          <CheckAvailabilityButton type="button" onClick={handleNicknameAvailabilityCheck}>
+            Check Nickname Availability
+          </CheckAvailabilityButton>
+
+          {nicknameAvailability !== null && (
+            <ResultMessage success={nicknameAvailability}>
+              {nicknameAvailability ? 'Nickname is available!' : 'Nickname is already taken!'}
+            </ResultMessage>
+          )}
         </FormGroup>
-        <FormGroup>
-          <Label className="input-file-button" for="input-file">
+        <Button type="submit">Create an Account</Button>
+      </form>
+      <p style={{ fontSize: '0.8em', color: 'gray' }}>
+        Already have an account? <LinkStyled to="/login">Login</LinkStyled>
+      </p>
+    </FormContainer>
+  );
+};
+
+export default SignupPage;
+
+
+{/* <FormGroup>
+          <Label className="input-file-button" htmlFor="input-file">
             <img
               src="image-regular.svg"
               width="20"
@@ -222,15 +306,4 @@ const SignupPage = () => {
               width="250"
               height="250"
             />
-          </FormGroup>
-        )}
-        <Button type="submit">Create an Account</Button>
-      </form>
-      <p style={{ fontSize: '0.8em', color: 'gray' }}>
-        Already have an account? <LinkStyled to="/login">Login</LinkStyled>
-      </p>
-    </FormContainer>
-  );
-};
-
-export default SignupPage;
+          </FormGroup> */}
