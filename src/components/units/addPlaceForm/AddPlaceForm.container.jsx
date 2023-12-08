3d8bd9d0f92f292605c04e60c8a5da9a23d69a86
jjunohj <jjunohj@gmail.com>
2023-12-08 14:35:17 +0900

    chore: 장소 등록 시 파일 업로드, 에러 처리, 모달 알림 구현

--- src/components/units/addPlaceForm/AddPlaceForm.container.jsx
+++ src/components/units/addPlaceForm/AddPlaceForm.container.jsx
@@ -1,16 +1,18 @@
 import AddPlaceFormUI from './AddPlaceForm.presenter';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import useModal from '@hooks/useModal';
 
 /** 한글 장소명, 영어 장소명, 간단한 영어 소개, 간판 이미지, 한글 주소, 영어 주소, 키오스크 유무, 주차장 유무, 영어 가능, 화장실 유무 폼 제출 */
 export default function AddPlaceForm(props) {
   const [preview, setPreview] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
+  const { openModal, closeModal } = useModal();
   const { register, handleSubmit, watch, errors, control } = useForm();
 
   // POST http://43.200.181.183:8000/locations
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     const msgBody = {
       koName: props.placeName,
       koAddress: props.placeAddress,
@@ -20,22 +22,42 @@ export default function AddPlaceForm(props) {
       wifiAvailable: data.wifiAvailable,
       description: data.placeDescription,
       category: codeToCategory(props.currCategory),
-      // image: selectedFile,
-      image: 'tempFile',
+      image: selectedFile,
       latitude: props.placeLatLng.latitude,
       longitude: props.placeLatLng.longitude,
     };
-    alert(JSON.stringify(msgBody));
 
-    fetch('http://43.200.181.183:8000/locations', {
-      method: 'POST',
-      body: JSON.stringify(msgBody),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => res.json())
-      .catch((err) => console.error(err));
+    console.log(typeof selectedFile);
+
+    try {
+      const response = await fetch(
+        'http://15.165.42.212:3000/api/exchangers/v1/locations',
+        {
+          method: 'POST',
+          body: JSON.stringify(msgBody),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        },
+      );
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+
+      const modalData = {
+        title: 'Add Place By Kakao API',
+        content: 'Succesfully added a place!',
+      };
+      openModal(modalData);
+
+      const result = await response.json();
+      console.log('result', result);
+    } catch (error) {
+      closeModal();
+      alert('Failed to add a place !');
+      console.log(error);
+    }
   };
 
   const codeToCategory = (category) => {
