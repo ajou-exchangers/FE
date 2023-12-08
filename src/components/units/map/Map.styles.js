ecb4d3d79fa00f73cda891b6defd90d4825fe825
jjunohj <jjunohj@gmail.com>
2023-12-08 14:37:45 +0900

    feat: 검색 모듈 분리 -> 검색 장소 핀 표시, 시점 이동 구현

--- src/components/units/map/Map.styles.js
+++ src/components/units/map/Map.styles.js
@@ -4,6 +4,7 @@ export const MapContainer = styled.div`
   position: relative;
   width: 100%;
   height: 100vh;
+  z-index: 1;
 `;
 
 export const AddPlaceButton = styled.button`
@@ -21,45 +22,5 @@ export const AddPlaceButton = styled.button`
   cursor: pointer;
   border: none;
   outline: none;
-  z-index: 111;
-`;
-
-export const SearchWrapper = styled.div`
-  position: absolute;
-  top: 5rem;
-  left: 2rem;
   z-index: 10;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 450px;
-  height: 60px;
-  border: 1px solid #e5e5e5;
-  border-radius: 30px;
-  background-color: #fff;
-  box-shadow: 1px 1px 10px 3px rgba(0, 0, 0, 0.1);
-  padding: 0 1rem;
-
-  @media (max-width: 768px) {
-    width: 300px;
-  }
-`;
-
-export const SearchInput = styled.input`
-  width: 80%;
-  height: 80%;
-  border: none;
-  outline: none;
-  font-size: 1.2rem;
-  font-weight: 400;
-`;
-
-export const SearchButton = styled.button`
-  background: url('magnifying-glass-solid.svg') no-repeat;
-  width: 25px;
-  height: 25px;
-  border: none;
-  back &:hover {
-    cursor: pointer;
-  }
 `;
